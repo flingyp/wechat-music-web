f88d9dd893a80ba415dbcfab1b9056013493c804
flingyp <1213563369@qq.com>
2020-05-08 10:13:58 +0800

    歌单页面获取和加载更多功能

--- src/views/playlist/list.vue
+++ src/views/playlist/list.vue
@@ -1,12 +1,58 @@
 <template>
     <div>
-        歌单
+        <el-table v-loading="loading" :data="playlist" stripe>
+            <el-table-column type="index" width="50"></el-table-column>
+            <el-table-column label="封面" width="100">
+                <template slot-scope="scope">
+                    <img :src="scope.row.picUrl" alt height="50" />
+                </template>
+            </el-table-column>
+            <el-table-column prop="name" label="名称"></el-table-column>
+            <el-table-column prop="copywriter" label="描述"></el-table-column>
+            <el-table-column label="操作">
+                <template>
+                    <el-button size="mini">编辑</el-button>
+                    <el-button size="mini" type="danger">删除</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 
 <script>
+import {fetchList} from '@/api/playlist'
+import scroll from '@/utils/scroll'
 export default {
-
+    data () {
+        return {
+            playlist: [],
+            count: 50,
+            loading: false      
+        }
+    },
+    created () {
+        this.getList()
+    },
+    mounted () {
+        scroll.start(this.getList)
+    },
+    methods: {
+        getList() {
+            this.loading = true
+            fetchList({
+                start: this.playlist.length,
+                count: this.count
+            }).then(res => {
+                // console.log(res)
+                this.playlist = this.playlist.concat(res.data)
+                // 如果取到的数据数量 已经少于 count 代表下一次没有新的数据了
+                if(res.data.length < this.count) {
+                    scroll.end()
+                }
+                this.loading = false
+            })
+        }
+    }
 }
 </script>
 
