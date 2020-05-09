519ba6478e28185a2b58f7220dcc98bc3c2bde0c
flingyp <1213563369@qq.com>
2020-05-09 11:18:57 +0800

    轮播图数据管理包括上传和删除

--- src/router/index.js
+++ src/router/index.js
@@ -65,8 +65,8 @@ export const constantRoutes = [
     component: Layout,
     children: [
       {
-        path: 'swiper',
-        component: () => import('@/views/swiper/swiper'),
+        path: 'list',
+        component: () => import('@/views/swiper/list'),
         meta: { title: '轮播图管理', icon: 'example' }
       }
     ]
